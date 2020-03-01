e5fc7f7077326d84d188623ee68896cea537c2d1
watacle <sirwatacle@gmail.com>
2020-03-01 12:07:33 +0200

    created auth middleware

---
+++ middleware/auth.js
@@ -0,0 +1,21 @@
+   
+const jwt = require('jsonwebtoken')
+require('dotenv').config();
+
+
+const auth = (req,res,next)=>{
+  const token = req.headers('auth-token')
+  if(!token){
+     res.status(401).json({msg:'not authenticated'})
+  }
+  try {
+      const verified = jwt.verify(token, process.env.JWTSecret)
+      req.user = verified;
+      next()
+
+  } catch (error) {
+      res.status(404).json({error:'session expired/invalid token'})
+  }
+}
+
+module.exports = auth;
--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const mongoose = require('mongoose')
-const colors = require('colors')
+require('colors')
 const PORT = process.env.PORT | 3002
 require('dotenv').config()
 
